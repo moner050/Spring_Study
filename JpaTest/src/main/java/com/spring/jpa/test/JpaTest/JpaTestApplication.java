67b8bd7a943e86e0490de07a1d68898a82563cf7
moner050 <83222976+moner050@users.noreply.github.com>
2021-10-30 23:22:22 +0900

    JAVA Spring JPA 테스트

---
+++ JpaTest/src/main/java/com/spring/jpa/test/JpaTest/JpaTestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.test.JpaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTestApplication.class, args);
+	}
+
+}
---
+++ JpaTest/src/test/java/com/spring/jpa/test/JpaTest/JpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.test.JpaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
