d1576ac171ae540eb656b110f3698c9df4c3a47b
moner050 <83222976+moner050@users.noreply.github.com>
2022-03-10 16:11:15 +0900

    Spring RestTemplate 이용하여 client와 server 통신 실습

---
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,142 @@
+package com.example.client.service;
+
+import com.example.client.dto.Req;
+import com.example.client.dto.UserRequest;
+import com.example.client.dto.UserResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+    // http://localhost/api/server/hello
+    // response 받아오기
+
+    public UserResponse hello()
+    {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .queryParam("name", "stevess")
+                .queryParam("age", 99)
+                .encode()
+                .build()
+                .toUri();
+
+        // 주소가 잘 만들어졌나 확인
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+//        String result = restTemplate.getForObject(uri, String.class);
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+
+    public void post()
+    {
+        // http://localhost:9090/api/server/user/{userId}/name/{userName}
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+        System.out.println(uri);
+
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(uri, req, String.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+//        return response.getBody();
+    }
+
+    public UserResponse exchange()
+    {
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+        System.out.println(uri);
+
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RequestEntity<UserRequest> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization", "abcd")
+                .header("custom-header", "ffffff")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.exchange(requestEntity, UserResponse.class);
+
+        return response.getBody();
+    }
+
+    public Req<UserResponse> genericExchange()
+    {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+        System.out.println(uri);
+
+        UserRequest userRequest = new UserRequest();
+        userRequest.setName("steve");
+        userRequest.setAge(10);
+
+        Req<UserRequest> req = new Req<>();
+        req.setHeader(
+            new Req.Header()
+        );
+
+        req.setResBody(
+            userRequest
+        );
+
+        RequestEntity<Req<UserRequest>> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization", "abcd")
+                .header("custom-header", "ffffff")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        new ParameterizedTypeReference<Req<UserResponse>>(){};
+
+        ResponseEntity<Req<UserResponse>> response
+                = restTemplate.exchange(requestEntity, new ParameterizedTypeReference<Req<UserResponse>>(){});
+
+        return response.getBody();
+    }
+}
---
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -0,0 +1,50 @@
+package com.example.server.controller;
+
+import com.example.server.dto.Req;
+import com.example.server.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/server")
+public class ServerApiController {
+
+
+    @GetMapping("/hello")
+    public User hello(@RequestParam String name, @RequestParam int age)
+    {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        return user;
+    }
+
+    @PostMapping("/user/{userId}/name/{userName}")
+    public Req<User> post(
+//            HttpEntity<String> entity,
+            @RequestBody Req<User> user,
+                     @PathVariable int userId,
+                     @PathVariable String userName,
+                     @RequestHeader("x-authorization") String authorization,
+                     @RequestHeader("custom-header") String customHeader
+    )
+    {
+//        log.info("req : {}", entity);
+        log.info("userId : {}, userName : {}", userId, userName);
+        log.info("authorization : {}, custom-header : {}", authorization, customHeader);
+        log.info("client req : {}",user);
+
+        Req<User> response = new Req<>();
+        response.setHeader(
+                new Req.Header()
+        );
+        response.setResBody(user.getResBody());
+
+        return response;
+    }
+}
