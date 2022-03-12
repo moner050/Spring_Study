1da9bd597254ccbb8b65f48b4b149fab94ff775c
moner050 <83222976+moner050@users.noreply.github.com>
2022-03-12 12:46:16 +0900

    Spring JUnit에 Mockito 를 사용해 테스트해보기.

---
+++ calculator/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String args[])
+    {
+        System.out.println("hello junit");
+
+        MarketApi marketApi = new MarketApi();
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+
+        System.out.println(calculator.sum(10, 10));
+    }
+}
