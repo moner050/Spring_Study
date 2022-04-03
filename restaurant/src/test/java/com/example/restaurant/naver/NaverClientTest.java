d35cb8e3d552550389f83db49ab14c064bb9abb5
moner050 <minhyung0237@gmail.com>
2022-04-03 18:45:28 +0900

    test: Add WishListService Test Code
    지역검색 및 이미지검색 테스트코드 작성

--- restaurant/src/test/java/com/example/restaurant/naver/NaverClientTest.java
+++ restaurant/src/test/java/com/example/restaurant/naver/NaverClientTest.java
@@ -2,6 +2,7 @@ package com.example.restaurant.naver;
 
 import com.example.restaurant.naver.dto.SearchImageReq;
 import com.example.restaurant.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,9 @@ public class NaverClientTest {
         search.setQuery("갈비집");
 
         var result =  naverClient.searchLocal(search);
+
+        // 카테고리의 값이 Null 값이면 안된다.
+        Assertions.assertNotNull(result.getItems().stream().findFirst().get().getCategory());
         System.out.println(result);
     }
 
--- restaurant/src/test/java/com/example/restaurant/wishList/service/WishListServiceTest.java
+++ restaurant/src/test/java/com/example/restaurant/wishList/service/WishListServiceTest.java
@@ -1,2 +1,24 @@
-package com.example.restaurant.wishList.service;public class WishListServiceTest {
+package com.example.restaurant.wishList.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WishListServiceTest {
+
+    @Autowired
+    private WishListService wishListService;
+
+    @Test
+    public void searchTest()
+    {
+        var result = wishListService.search("갈비집");
+
+        System.out.println(result);
+
+        // result 는 Null 값이면 안된다.
+        Assertions.assertNotNull(result);
+    }
 }
