1c1cc86c6fb84789874bf298e125e3c267daf1eb
moner050 <minhyung0237@gmail.com>
2022-04-17 02:09:12 +0900

    feat: Add add and findAll
    위시리스트 추가 및 전체 리스트 가져오기 추가

--- restaurant/src/main/java/com/example/restaurant/controller/ApiController.java
+++ restaurant/src/main/java/com/example/restaurant/controller/ApiController.java
@@ -1,2 +1,38 @@
-package com.example.restaurant.controller;public class ApiController {
+package com.example.restaurant.controller;
+
+import com.example.restaurant.wishList.dto.WishListDto;
+import com.example.restaurant.wishList.service.WishListService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/restaurant")
+@RequiredArgsConstructor
+public class ApiController {
+
+    private final WishListService wishListService;
+
+    @GetMapping("/search")
+    public WishListDto search(@RequestParam String query){
+        return wishListService.search(query);
+    }
+
+    @PostMapping("")
+    public WishListDto add(@RequestBody WishListDto wishListDto)
+    {
+        log.info("{}", wishListDto);
+
+        return wishListService.add(wishListDto);
+    }
+
+    // 전체 리스트 가져오기
+    @GetMapping("/all")
+    public List<WishListDto> findAll()
+    {
+        return wishListService.findAll();
+    }
 }
