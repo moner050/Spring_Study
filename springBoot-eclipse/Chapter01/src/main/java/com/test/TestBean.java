a55d677a55aee6be4762561a36bc324088ad9231
moner050 <minhyung0237@gmail.com>
2022-06-29 17:12:37 +0900

    feat: Create Chapter01
    http 요청이 들어오면 body내용으로 매핑해 리턴되는 값을 JSON형태로
    브라우저에 출력해주기.

---
+++ springBoot-eclipse/Chapter01/src/main/java/com/test/TestBean.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestBean {
+
+	public TestBean() {
+		System.out.println("===> TestBean 생성");
+	}
+	
+}
