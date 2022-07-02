18bf9804925a47aae0c765f7362f56eff00b7dc0
moner050 <minhyung0237@gmail.com>
2022-07-02 12:09:15 +0900

    feat: Create SameBeanFind & ExtendsFind Test, xml config
    부모타입으로 조회해 빈 찾기, 같은 타입의 빈 찾기, xml 기반의 스프링 설정 해보기

---
+++ core/src/test/java/my/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package my.core.xml;
+
+import my.core.member.Member;
+import my.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+
+}
