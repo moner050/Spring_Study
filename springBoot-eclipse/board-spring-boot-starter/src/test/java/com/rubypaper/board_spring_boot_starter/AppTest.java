ebd7d54cc7aba3d775e8974f594ac9510e886498
moner050 <minhyung0237@gmail.com>
2022-07-08 14:25:33 +0900

    feat: Create Custom Starter
    JDBC 및 Lombok 을 이용하기 위한 Custom Starter

---
+++ springBoot-eclipse/board-spring-boot-starter/src/test/java/com/rubypaper/board_spring_boot_starter/AppTest.java
@@ -0,0 +1,20 @@
+package com.rubypaper.board_spring_boot_starter;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
