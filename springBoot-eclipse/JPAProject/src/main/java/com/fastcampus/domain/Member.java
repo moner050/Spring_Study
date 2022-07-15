e691e0660e0a8776e91a8ad2df77601a41b0b02f
moner050 <minhyung0237@gmail.com>
2022-07-15 15:06:21 +0900

    feat: Create JPQL & JPA in MemberRepository
    JPQL과 JPQ 테스트해보기

--- springBoot-eclipse/JPAProject/src/main/java/com/fastcampus/domain/Member.java
+++ springBoot-eclipse/JPAProject/src/main/java/com/fastcampus/domain/Member.java
@@ -1,10 +1,17 @@
 package com.fastcampus.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -20,4 +27,6 @@ public class Member {
 	
 	private String city;
 
+//	@OneToMany(mappedBy = "member", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+//	private List<Order> orders = new ArrayList<>();
 }
