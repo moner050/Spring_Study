84b7b667e548a570315090b5fac3369c1ce28203
moner050 <minhyung0237@gmail.com>
2022-09-15 23:10:58 +0900

    test: Create OrderServiceImplTest
    맴버의 주문을 생성해서 등급에 따른 할인률이 정확히 측정되나 테스트

---
+++ core/src/test/java/my/core/order/OrderServiceImplTest.java
@@ -0,0 +1,22 @@
+package my.core.order;
+
+import my.core.discount.FixDiscountPolicy;
+import my.core.member.Grade;
+import my.core.member.Member;
+import my.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "김도", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
