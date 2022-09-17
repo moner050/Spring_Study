9b08290178606ec8bf07ae58ef20ad43da10a0f1
moner050 <minhyung0237@gmail.com>
2022-09-17 15:19:44 +0900

    test: add memberRepository bean address
    OrderServiceImpl 에서 memberRepository 의 Bean 을 꺼내와서 주소 출력하기 추가

--- core/src/test/java/my/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/my/core/scan/AutoAppConfigTest.java
@@ -1,7 +1,9 @@
 package my.core.scan;
 
 import my.core.AutoAppConfig;
+import my.core.member.MemberRepository;
 import my.core.member.MemberService;
+import my.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,6 +16,10 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 
 }
