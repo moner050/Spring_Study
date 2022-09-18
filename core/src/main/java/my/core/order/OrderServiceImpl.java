e9be45f179bc35ce0b217e575d38a1d8ef8d3102
moner050 <minhyung0237@gmail.com>
2022-09-18 17:35:46 +0900

    refactor: Using @RequiredArgsConstructor
    @RequiredArgsConstructor 를 사용해 간편하게 생성자 주입방식 사용

--- core/src/main/java/my/core/order/OrderServiceImpl.java
+++ core/src/main/java/my/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package my.core.order;
 
+import lombok.RequiredArgsConstructor;
 import my.core.discount.DiscountPolicy;
 import my.core.member.Member;
 import my.core.member.MemberRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
@@ -49,15 +51,14 @@ public class OrderServiceImpl implements OrderService{
 //        this.discountPolicy = discountPolicy;
 //    }
 
-
     // 생성자 주입 방식
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository,
-        DiscountPolicy discountPolicy) {
-        System.out.println("1. OrderServiceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository,
+//        DiscountPolicy discountPolicy) {
+//        System.out.println("1. OrderServiceImpl");
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
