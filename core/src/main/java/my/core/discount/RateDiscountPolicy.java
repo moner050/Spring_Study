7b3fb335b998fb2ec4f3176e153c37df07d8a8f7
moner050 <minhyung0237@gmail.com>
2022-10-08 19:44:38 +0900

    feat: remove @Qualifier Annotation & Add @Primary Annotation
    @Primary 로 우선권을 주는 방식 추가

--- core/src/main/java/my/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/my/core/discount/RateDiscountPolicy.java
@@ -3,10 +3,11 @@ package my.core.discount;
 import my.core.member.Grade;
 import my.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
