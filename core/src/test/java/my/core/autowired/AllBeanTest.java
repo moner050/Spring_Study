50f7823855961d3b20fb9f050be8c3f37dcbb21f
moner050 <minhyung0237@gmail.com>
2022-10-17 17:09:56 +0900

    test: Create AllBeanTest
    List와 Map을 사용해서 모든 스프링 빈을 주입받아 넘어오는 할인 정책에 따라 유동적으로 빈을 주입시켜주기 테스트

---
+++ core/src/test/java/my/core/autowired/AllBeanTest.java
@@ -0,0 +1,55 @@
+package my.core.autowired;
+
+import my.core.AutoAppConfig;
+import my.core.discount.DiscountPolicy;
+import my.core.member.Grade;
+import my.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Test
+    @DisplayName("모든 DiscountPolicy를 Map에 넣어 넘어오는 정책명에 따라서 할인정책 다르게 적용되나 테스트 해보기")
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+        // Map 에 모든 DiscountPolicy 를 주입받는다.
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        // discountCode 로 fixDiscountPolicy 가 넘어오면 map 에서 fixDiscountPolicy 스프링 빈을 찾아서 실행시켜준다.
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+
+            System.out.println("discountCode = " + discountCode);
+            System.out.println("discountPolicy = " + discountPolicy);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
