28eefee862d932b0c14c5c410715cc2f892da5be
moner050 <minhyung0237@gmail.com>
2022-10-18 20:11:25 +0900

    test: Create NetworkClient
    스프링 빈 생명주기 테스트를 위한 테스트용 네트워크 클라이언트 생성

---
+++ core/src/test/java/my/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package my.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    // 스프링 빈의 이벤트 라이프사이클
+    // 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("https://hello-spring.io");
+            return networkClient;
+        }
+    }
+}
