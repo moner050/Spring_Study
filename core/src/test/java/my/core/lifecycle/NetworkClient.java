438bf91b9e88cac23fd50c1da98532c0137e1ba9
moner050 <minhyung0237@gmail.com>
2022-10-18 20:14:31 +0900

    test: implements InitializingBean, DisposableBean
    스프링 전용 인터페이스들을 이용해서 의존관계 주입 전, 빈 소멸 전에 호출되는 메서드 오버라이드

--- core/src/test/java/my/core/lifecycle/NetworkClient.java
+++ core/src/test/java/my/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package my.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출 url : " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,20 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disConnect() {
         System.out.println("close: " + url);
+    }
+
+    // 의존관계 주입이 끝나면 호출된다.
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈 소멸 전에 호출된다.
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disConnect();
     }
 }
