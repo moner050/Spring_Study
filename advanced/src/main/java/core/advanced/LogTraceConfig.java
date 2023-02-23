e27e4aeb21d1fe770fe1277fbc8cca36458dea6b
moner050 <minhyung0237@gmail.com>
2023-02-23 22:24:10 +0900

    Feat: 로그추적기를 FieldLogTrace -> ThreadLocalLogTrace 로 변경

--- advanced/src/main/java/core/advanced/LogTraceConfig.java
+++ advanced/src/main/java/core/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package core.advanced;
 
-import core.advanced.trace.logtrace.FieldLogTrace;
 import core.advanced.trace.logtrace.LogTrace;
+import core.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
