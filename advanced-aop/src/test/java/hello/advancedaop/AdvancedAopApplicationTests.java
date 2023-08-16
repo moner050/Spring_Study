157304b16d08e38f77310f6f791a0e8efc0daeef
moner050 <minhyung0237@gmail.com>
2023-08-16 21:42:09 +0900

    Feat: Create advanced AOP Project
    Spring AOP 를 구현해보는 프로젝트 생성

---
+++ advanced-aop/src/test/java/hello/advancedaop/AdvancedAopApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.advancedaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
