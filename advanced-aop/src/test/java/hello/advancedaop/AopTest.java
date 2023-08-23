a11675ade6b47daadf2e5c9262be0d313bf0f1f8
moner050 <minhyung0237@gmail.com>
2023-08-23 22:46:08 +0900

    Test: AspectV3 Bean 등록

--- advanced-aop/src/test/java/hello/advancedaop/AopTest.java
+++ advanced-aop/src/test/java/hello/advancedaop/AopTest.java
@@ -4,6 +4,7 @@ import hello.advancedaop.order.OrderRepository;
 import hello.advancedaop.order.OrderService;
 import hello.advancedaop.order.aop.AspectV1;
 import hello.advancedaop.order.aop.AspectV2;
+import hello.advancedaop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 @SpringBootTest
 //@Import(AspectV1.class)
-@Import(AspectV2.class)
+//@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopTest {
 
     @Autowired
