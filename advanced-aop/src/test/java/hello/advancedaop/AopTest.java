706b8847efb203a772e301ae6a41157ce2a6b9dd
moner050 <minhyung0237@gmail.com>
2023-08-25 23:17:42 +0900

    Test: AspectV4Pointcut Bean 등록
    AspectV4Pointcut 을 테스트하기 위해 빈 등록

--- advanced-aop/src/test/java/hello/advancedaop/AopTest.java
+++ advanced-aop/src/test/java/hello/advancedaop/AopTest.java
@@ -5,6 +5,7 @@ import hello.advancedaop.order.OrderService;
 import hello.advancedaop.order.aop.AspectV1;
 import hello.advancedaop.order.aop.AspectV2;
 import hello.advancedaop.order.aop.AspectV3;
+import hello.advancedaop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
 
     @Autowired
