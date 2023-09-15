b31b7dadc591dcd58008237393665620e076d16a
moner050 <minhyung0237@gmail.com>
2023-09-15 22:54:17 +0900

    Test: Create AtAnnotationTest
    @annotation 을 이용해 메서드가 주어진 애노테이션을 가지고 있는 조인 포인트를 매칭시키는 테스트 생성

---
+++ advanced-aop/src/test/java/hello/advancedaop/pointcut/AtAnnotationTest.java
@@ -0,0 +1,40 @@
+package hello.advancedaop.pointcut;
+
+import hello.advancedaop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(AtAnnotationTest.AtAnnotationAspect.class)
+@SpringBootTest
+public class AtAnnotationTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    /**
+     * 메서드가 주어진 애노테이션을 가지고 있는 조인 포인트를 매칭
+     */
+    @Slf4j
+    @Aspect
+    static class AtAnnotationAspect {
+        @Around("@annotation(hello.advancedaop.member.annotation.MethodAop)")
+        public Object doAtAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@annotation] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+    }
+}
