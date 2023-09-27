24492fc1176e82b046f61b07c9b7544b96cf6dbd
moner050 <minhyung0237@gmail.com>
2023-09-27 23:13:40 +0900

    Feat: Add Trace Annotation
    Trace 어노테이션을 걸어 로그를 출력하게 만들어줌

--- advanced-aop/src/main/java/hello/advancedaop/exam/ExamService.java
+++ advanced-aop/src/main/java/hello/advancedaop/exam/ExamService.java
@@ -1,5 +1,6 @@
 package hello.advancedaop.exam;
 
+import hello.advancedaop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ public class ExamService {
 
     private final ExamRepository examRepository;
 
+    @Trace
     public void request(String itemId) {
         examRepository.save(itemId);
     }
