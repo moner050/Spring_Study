a23a57f8c4ddbe838cf787e93335f3202e24a6b9
moner050 <minhyung0237@gmail.com>
2023-09-27 23:11:55 +0900

    Feat: Create TraceAspect
    Trace 어노테이션을 사용하는 모든 메소드의 로그를 출력하는 클래스 생성

---
+++ advanced-aop/src/main/java/hello/advancedaop/exam/aop/TraceAspect.java
@@ -0,0 +1,18 @@
+package hello.advancedaop.exam.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class TraceAspect {
+
+    // 시그니처도 남기고 인수정보도 로그로 남긴다.
+    @Before("@annotation(hello.advancedaop.exam.annotation.Trace)")
+    public void doTrace(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+    }
+}
