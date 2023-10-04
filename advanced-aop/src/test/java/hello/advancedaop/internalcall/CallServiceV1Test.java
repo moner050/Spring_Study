28883586dfce08d32a91c968497faaa7e2f7452f
moner050 <minhyung0237@gmail.com>
2023-10-04 22:29:09 +0900

    Test: Create CallServiceV1Test
    자기 자신을 주입하여 자기 자신의 인스턴스를 호출하는것이 아닌 프록시 인스턴스를 호출하는 테스트 생성

---
+++ advanced-aop/src/test/java/hello/advancedaop/internalcall/CallServiceV1Test.java
@@ -0,0 +1,26 @@
+package hello.advancedaop.internalcall;
+
+import hello.advancedaop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV1Test {
+
+    // 자기자신 주입 방식으로 메서드를 호출 시 자기 자신의 인스턴스를 호출하는것이 아닌 프록시 인스턴스를 통해 호출하기.
+    @Autowired
+    CallServiceV1 callServiceV1;
+
+    @Test
+    void external() {
+        callServiceV1.external();
+    }
+
+}
