1254f423005cfc483878e6c91bb2e51957ccc7fb
moner050 <minhyung0237@gmail.com>
2023-10-27 23:01:21 +0900

    Test: 일반 DriverManager 와 DriverManagerDataSource 를 통해 커넥션을 획득하는 방식의 차이점 테스트하기

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,39 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        // 설정과 사용을 분리시켜 향후 변경에 더 유연하게 대처할 수 있고, 설정은 신경쓰지 않고 메서드를 호출만 해 사용하면 된다.
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+}
