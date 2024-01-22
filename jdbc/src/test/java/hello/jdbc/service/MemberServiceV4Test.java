b17887254c7c6c1e90b3cafe0298c7c166612ac6
moner050 <minhyung0237@gmail.com>
2024-01-22 23:08:02 +0900

    Test: 스프링 예외 추상화를 적용한 레파지토리 빈 적용.

--- jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -3,6 +3,7 @@ package hello.jdbc.service;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -49,13 +50,14 @@ class MemberServiceV4Test {
         }
 
         @Bean
-        MemberRepository memberRepositoryV4_1() {
-            return new MemberRepositoryV4_1(dataSource);
+        MemberRepository memberRepository() {
+//            return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV4_2(dataSource);
         }
 
         @Bean
         MemberServiceV4 memberServiceV4() {
-            return new MemberServiceV4(memberRepositoryV4_1());
+            return new MemberServiceV4(memberRepository());
         }
     }
 
